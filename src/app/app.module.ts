eb612416807d531548780db658c07ebebf8a64e3
Shannon Lal <shannonlal@hotmail.com>
2020-09-23 19:22:04 -0400

    Initial code for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryViewComponent } from './components/summary-view/summary-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryViewComponent
   ],
   imports: [
     BrowserModule,
