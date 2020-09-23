a8403f146972bbbc1d9b967992134f9b7def135d
Shannon Lal <shannonlal@hotmail.com>
2020-09-23 19:17:39 -0400

    Clean up test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('jest-sample');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jest-sample app is running!');
-  });
 });
